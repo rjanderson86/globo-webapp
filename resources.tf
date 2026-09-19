eb7fdd9a03095dfdf6ce97e190eef3df8ab96aa4
Ronald Anderson <ronaldwandersonjr@gmail.com>
2026-09-19 07:04:43 -0700

    Switch to using terraform_data

--- resources.tf
+++ resources.tf
@@ -50,12 +50,11 @@ resource "aws_instance" "main" {
 
 }
 
-resource "null_resource" "webapp" {
-
-  triggers = {
-    webapp_server_count = length(aws_instance.main.*.id)
-    web_server_names    = join(",", aws_instance.main.*.id)
-  }
+resource "terraform_data" "webapp" {
+  triggers_replace = [
+    length(aws_instance.main.*.id),
+    join(",", aws_instance.main.*.id)
+  ]
 
   provisioner "file" {
     content = templatefile("./templates/application.config.tpl", {
@@ -73,7 +72,6 @@ resource "null_resource" "webapp" {
     host        = aws_instance.main[0].public_ip
     private_key = module.ssh_keys.private_key_openssh
   }
-
 }
 
 resource "aws_lb" "main" {
